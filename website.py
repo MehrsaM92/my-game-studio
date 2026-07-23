269ec78f0dc0bb93a60a18c249899433ebcd50e9
MehrsaM92 <ahmad.beigi.mehrsam@gmail.com>
2026-07-23 17:20:42 +0330

    Update website.py

--- website.py
+++ website.py
@@ -14,6 +14,11 @@ HTML = """
 
 <meta charset="UTF-8">
 
+<meta name="google-site-verification" content="ULKlsCJLxKZPZcj4H5NkXbxu0p8OkCf2ioaLwIFockY" />
+
+<meta name="viewport"
+content="width=device-width, initial-scale=1.0">
+
 <meta name="viewport"
 content="width=device-width, initial-scale=1.0">
 
